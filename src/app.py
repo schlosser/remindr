f051c4fc236e58622cd4752b3e7b8d97302f5365
Nate Brennand <natebrennand@gmail.com>
2013-09-06 21:59:22 -0400

    more config

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 
+# libraries
 from flask.ext.pymongo import PyMongo
 from flask import Flask, make_response
+
 
 app = Flask(__name__)
 app.config.from_object('config.flask_config')
---
+++ src/config/test_config.sh
@@ -0,0 +1,15 @@
+
+# mongo settings
+export MONGO_HOST="localhost"
+export MONGO_PORT="27017"
+export MONGO_DBNAME="reminder"
+export MONGO_USERNAME="test_user"
+export MONGO_PASSWORD="test_mongo"
+
+# flask settings
+export DEBUG="TRUE"
+export SECRET_KEY="e150f65d825839ea131003c99ab817a6b863ea1dfad2d5330d8fcc010385ac2c5dc8d16446e4b36750a24150209bc1e971d2bc0ce982fdc2a69a7581185f4944"
+
+# misc settings
+export HOST_URL="http://localhost:5000"
+
