7b06db66468176c4c8b6950381656bc19efb806a
Nate Brennand <natebrennand@gmail.com>
2013-09-07 02:45:30 -0400

    flesh out signup.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -11,9 +11,9 @@ app.config( ['$routeProvider', function($routeProvider) {
 	$routeProvider
 
     // ROUTES
-    .when('/login', {templateUrl: 'static/partials/login.html',     controller: 'LoginController'})
-    .when('/signup', {templateUrl: 'static/partials/signup.html', controller: 'SignupController'})
-    .when('/',		{templateUrl: 'static/partials/home.html',		controller: 'HomeController'})
+    .when('/login',     {templateUrl: 'static/partials/login.html',     controller: 'LoginController'})
+    .when('/signup',    {templateUrl: 'static/partials/signup.html',    controller: 'SignupController'})
+    .when('/',          {templateUrl: 'static/partials/home.html',      controller: 'HomeController'})
 
 		// default
 		.otherwise({redirectTo: '/'});
--- src/static/js/controllers/signup.js
+++ src/static/js/controllers/signup.js
@@ -1,21 +1,30 @@
 
 angular.module('app.signup', [])
 
-.controller('SignupController', function($scope, $http, $location) {
-  $scope.user = {};
+.controller('SignupController', function($scope, $http, $location, flash) {
+	$scope.user = {};
 
-  $scope.signup = function () {
-    console.log($scope.user);
+	$scope.signup = function () {
+		var data = $scope.user;
+		if (!checkPass(data.password, data.repeatPassword)) {
+			flash.alertError('Passwords not matching');
+			return;
+		}
 
-    $http.post('/signup', $scope.user)
-      .success( function (resp) {
-        console.log('Sign up success');
-        console.log(resp);
-        $location.url('/');
-      })
-      .error( function (err) {
-        console.error('Sign up ERROR');
-        console.error(err);
-      });
-  };
+		$http.post('/signup', $scope.user)
+			.success( function (resp) {
+				flash.notify(resp.message);
+				$location.url('/');
+			})
+			.error( function (err) {
+				flash.alertError(err.message);
+			});
+	};
+
+	var checkPass = function (pass1, pass2) {
+		if (pass1 !== pass2) {
+			return false;
+		}
+		return true;
+	};
 });
