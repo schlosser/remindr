cfb4175c7e17282204b7dd4ca8e14d75a7d395cf
Nate Brennand <natebrennand@gmail.com>
2013-09-07 02:51:10 -0400

    spaces --> tabs

--- src/static/js/services/flash.js
+++ src/static/js/services/flash.js
@@ -2,36 +2,36 @@
 angular.module('app.flash', [])
 
 .factory('flash', function ($rootScope) {
-    var queue = [],
-        currentMessage = '';
+	var queue = [],
+		currentMessage = '';
 
-    $rootScope.$on('$routeChangeSuccess', function () {
-        if (queue.length > 0) {
-            currentMessage = queue.shift();
-        } else {
-            currentMessage = '';
-        }
-    });
+	$rootScope.$on('$routeChangeSuccess', function () {
+		if (queue.length > 0) {
+			currentMessage = queue.shift();
+		} else {
+			currentMessage = '';
+		}
+	});
 
-    return {
-        // will load on the next page
-        notify: function (message) {
-            queue.push(message);
-        },
-        // will load error on next page
-        notifyError: function (error) {
-            queue.push('ERROR: ' + error);
-        },
-        // loads on current page
-        alert: function (message) {
-            currentMessage = message;
-        },
-        // loads an error on the current page
-        alertError: function (error) {
-            currentMessage = 'ERROR: ' + error;
-        },
-        get: function (message) {
-            return currentMessage;
-        }
-    };
+	return {
+		// will load on the next page
+		notify: function (message) {
+			queue.push(message);
+		},
+		// will load error on next page
+		notifyError: function (error) {
+			queue.push('ERROR: ' + error);
+		},
+		// loads on current page
+		alert: function (message) {
+			currentMessage = message;
+		},
+		// loads an error on the current page
+		alertError: function (error) {
+			currentMessage = 'ERROR: ' + error;
+		},
+		get: function (message) {
+			return currentMessage;
+		}
+	};
 });
