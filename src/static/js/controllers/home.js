1e9ff0ccc1736a1bd9fa2bf2f648a11407cb7d2a
Dan Schlosser <dan.r.schlosser@gmail.com>
2013-09-07 03:03:00 -0400

    start html template

--- src/static/js/controllers/home.js
+++ src/static/js/controllers/home.js
@@ -2,7 +2,13 @@
 angular.module('app.home', [])
 
 .controller('HomeController', function ($scope) {
-    $scope.test = 'TEST';
+    $scope.message.targetUser = "";
+    $scope.message.subject = "";
+    $scope.message.time = "";
+    $scope.message.date = "";
+    $scope.message.body = "";
+
+
 });
 
 
